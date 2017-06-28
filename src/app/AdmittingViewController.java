a303a324c960e5acaf7699735eb5d11b1c12b199
Jennifer Mullen <redjen@gmail.com>
2017-06-27 17:36:12 -0700

    Updated view and controller for admitting patients

--- src/app/AdmittingViewController.java
+++ src/app/AdmittingViewController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -15,38 +16,122 @@ import javafx.scene.control.TextField;
  */
 public class AdmittingViewController implements Initializable {
 
-    // First Name TextField used by FXML
-    @FXML
-    private TextField firstName;
+   // First name field
+   @FXML
+   private TextField firstName;
 
-    // Last Name TextField used by FXML
-    @FXML
-    private TextField lastName;
+   // Last name field
+   @FXML
+   private TextField lastName;
 
-    // SendUpdates checkbox used by FXML
-    @FXML
-    private CheckBox sendUpdates;
+   // Checkbox to opt in or out to notifications
+   @FXML
+   private CheckBox sendUpdates;
 
-    // Clock Label used by FXML
-    @FXML
-    private Label clockLabel;
+   // Validation error message
+   @FXML
+   private Label errorMessage;
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }
+   // Patient added success message
+   @FXML
+   private Label successMessage;
 
-    /*
-        Method is called when the patient add button is clicked
-        @param ActionEvent event being sent from fxml click event
-     */
-    public void onPatientAdded(ActionEvent event) {
-        String firstNameText = firstName.getText();
-        String lastNameText = lastName.getText();
-        boolean sendUpdates = this.sendUpdates.isPressed();
-    }
+   // Add a new patient button
+   @FXML
+   private Button addNewButton;
+
+   // Clear the form button
+   @FXML
+   private Button clearButton;
+
+   // Add another patient after successful add
+   @FXML
+   private Button addAnotherButton;
+
+   // Patient ID label
+   @FXML
+   private Label patientIdTitleLabel;
+
+   // Patient ID
+   @FXML
+   private Label patientIdLabel;
+
+   /**
+    * {@inheritDoc }
+    */
+   @Override
+   public void initialize(URL url, ResourceBundle rb) {
+      // TODO
+   }
+
+   /**
+    * Handler for clicking the add button
+    *
+    * @param event event being sent from fxml click event
+    */
+   public void onAddClicked(ActionEvent event) {
+      addPatient();
+   }
+
+   /**
+    * clears the form, clearing all fields and resetting button visibility
+    *
+    * @param event
+    */
+   public void reset(ActionEvent event) {
+      firstName.clear();
+      firstName.setEditable(true);
+      lastName.clear();
+      patientIdLabel.setText("");
+      patientIdLabel.setVisible(false);
+      patientIdTitleLabel.setVisible(false);
+      firstName.setEditable(true);
+      lastName.setEditable(true);
+      addNewButton.setVisible(true);
+      clearButton.setVisible(true);
+      errorMessage.setVisible(false);
+      successMessage.setVisible(false);
+      addAnotherButton.setVisible(false);
+   }
+
+   /**
+    * Adds a new patient to the patient list.
+    */
+   private void addPatient() {
+      String firstNameText = firstName.getText();
+      String lastNameText = lastName.getText();
+      boolean updatesSelected = sendUpdates.isSelected();
+      long patientID = 99999;
+
+      if (!firstNameText.isEmpty() && !lastNameText.isEmpty()) {
+         // TODO call patient controller or model add
+         errorMessage.setVisible(false);
+
+         System.out.println(String.format("[new patient] firstName: %s, lastName: %s, updates selected: %b, id: %d",
+                 firstNameText, lastNameText, updatesSelected, patientID));
+         updateViewPatientAdded(Long.toString(patientID));
+      } else {
+         errorMessage.setVisible(true);
+      }
+   }
+
+   /**
+    * Sets the form state after successfully adding a patient by hiding editing
+    * controls, showing the success message, and showing the "add another" button
+    *
+    * @param patientId new patient's ID
+    */
+   private void updateViewPatientAdded(final String patientId) {
+      patientIdLabel.setText(patientId);
+      patientIdLabel.setVisible(true);
+      patientIdTitleLabel.setVisible(true);
+      firstName.setEditable(false);
+      lastName.setEditable(false);
+      addNewButton.setVisible(false);
+      clearButton.setVisible(false);
+      errorMessage.setVisible(false);
+      successMessage.setVisible(true);
+      addAnotherButton.setVisible(true);
+   }
 
 }
