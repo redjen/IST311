c45d43984a12a50a3f0387ce726255d2af3d7782
Jennifer Mullen <redjen@gmail.com>
2017-07-18 21:43:53 -0700

    Fire events when Patient objects in the PatientCollection observable list change

--- src/hospital/PatientCollection.java
+++ src/hospital/PatientCollection.java
@@ -4,8 +4,10 @@ import java.time.Instant;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.UUID;
+import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.util.Callback;
 
 /**
  * PatientCollection manages the list of patients in the system
@@ -16,9 +18,47 @@ import javafx.collections.ObservableList;
 public class PatientCollection {
 
    private final HashMap<String, Patient> patients = new HashMap<>();
-   private final ObservableList<Patient> patientList = FXCollections.observableArrayList();
+   private final ObservableList<Patient> patientList;
 
    public PatientCollection() {
+
+      /*
+      Creates an observable list for two purposes:
+      
+      1. An implicit listener updates the patient table view automatically
+      
+      2. Additional listeners can be (but are not required to be) attached to
+      listen for changes to the list or to the list elements. This allows us
+      to automatically save data to disk without having to update this or the
+      Patient model object and without needing to update the ViewManager
+      controller. Everything is in the dao package.
+      
+      If an object is added or removed the list will fire an event
+      that the saving data data controller can catch and use to add or archive 
+      the data for the object. 
+      
+      The parameter passed to the list constructor sets listeners on the
+      properties for each Patient object. Whenever one of the observed
+      properties changes an event is fired. The controller responsible for 
+      saving data (if any) is notified of the update and updates the object's
+      saved data. The list will not fire this event, which creates the need
+      for the extractor.
+      
+      We should only do this with the Patient properties that can change to
+      avoid firing events and writing data unnecessarily. When we add a status
+      field we'll need to figure out how to add that.
+       */
+      patientList = FXCollections.observableArrayList(new Callback<Patient, Observable[]>() {
+         @Override
+         public Observable[] call(Patient param) {
+            return new Observable[]{
+               param.getFirstNameProperty(),
+               param.getLastNameProperty()
+            // TODO add getting status, whenever we implement that
+            };
+         }
+      });
+
    }
 
    public void add(Patient p) {
